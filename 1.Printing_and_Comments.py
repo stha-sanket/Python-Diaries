2a1418131f1cc2cc303de72a8708f8eb98af3541
stha-sanket <sanketshrestha09@gmail.com>
2025-12-24 01:14:39 +0545

    learning printing

---
+++ 1.Printing_and_Comments.py
@@ -0,0 +1,6 @@
+# Lets start python with printing or seeing output after executing the code
+# try this
+print('hello world')
+
+# To run type python Starting_Python.py in the terminal with the same folder open
+# If you are facing issues type ls or dir to see all the files if you can see your .py file then you are doing good
