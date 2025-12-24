9be523858e3054cccce8af9aba1ffbe8f06c8302
stha-sanket <sanketshrestha09@gmail.com>
2025-12-24 23:11:43 +0545

    variables starting

---
+++ 2.Variables.py
@@ -0,0 +1,15 @@
+# Variables: these are used to store your data in python or hold your data
+# like in maths where we used x = 10 or y = 9, variables are same here in python too
+#learning to name variables:
+
+name = 'sanket' #using albhabet is valid
+user_id = 2 # using uderscore (_) to seperate words is valid
+num1 = 23 # using number after is valid
+
+# but
+
+'''
+we cannot use these:
+- 1name = 'sanket'(a variable name cannot start with a number)
+- last name = 'shrestha'(we cannot leave space in name called white space)
+'''
